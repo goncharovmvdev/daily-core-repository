661909f69165c4346c861c2389c9fa43aab92c8e
goncharovmvdev <goncharovmv.dev@gmail.com>
2025-09-06 17:00:12 +0300

    init:event feature

---
+++ src/main/kotlin/com/daily/core/repository/common/entity/CreatedAndUpdatedMetadata.kt
@@ -0,0 +1,17 @@
+package com.daily.core.repository.common.entity
+
+interface CreatedAndUpdatedMetadata {
+
+    val createdBy: String?
+    val updatedBy: String?
+    val createdAt: Long?
+    val updatedAt: Long?
+
+    data class DataImpl(
+
+        override val createdBy: String? = null,
+        override val updatedBy: String? = null,
+        override val createdAt: Long? = null,
+        override val updatedAt: Long? = null,
+    ): CreatedAndUpdatedMetadata
+}
---
+++ src/main/kotlin/com/daily/core/repository/event/entity/EventEntity.kt
@@ -0,0 +1,22 @@
+package com.daily.core.repository.event.entity;
+
+import com.daily.core.repository.common.entity.CreatedAndUpdatedMetadata
+import com.daily.core.repository.common.entity.Period
+
+interface EventEntity {
+
+    val id: String?
+    val title: String?
+    val description: String?
+    val period: Period?
+    val createdAndUpdatedMetadata: CreatedAndUpdatedMetadata?
+
+    data class DataImpl(
+
+        override val id: String? = null,
+        override val title: String? = null,
+        override val description: String? = null,
+        override val period: Period? = null,
+        override val createdAndUpdatedMetadata: CreatedAndUpdatedMetadata? = null,
+    ) : EventEntity
+}
