430410426d1fc27d84a8116894fc1f1cd3b6cc97
goncharovmvdev <goncharovmv.dev@gmail.com>
2025-09-06 21:58:26 +0300

    fix:publish pipeline

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,6 @@
 plugins {
     kotlin("jvm") version "2.1.10"
+    `maven-publish`
 }
 repositories {
     mavenCentral()
@@ -9,4 +10,23 @@ kotlin {
 }
 tasks.wrapper {
     gradleVersion = "8.10"
+}
+publishing {
+    repositories {
+        maven {
+            name = "GitHubPackages"
+            url = uri("https://maven.pkg.github.com/goncharovmvdev/daily-core-repository")
+            credentials {
+                username = project.findProperty("gpr.user") as String?
+                    ?: System.getenv("USERNAME")
+                password = project.findProperty("gpr.key") as String?
+                    ?: System.getenv("TOKEN")
+            }
+        }
+    }
+    publications {
+        register<MavenPublication>("gpr") {
+            from(components["java"])
+        }
+    }
 }
